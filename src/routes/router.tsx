9d7104df17d7e5ef06a8ac55389d666e180bd337
Mosi <mosisu07@gmail.com>
2024-05-06 14:40:32 +0800

    update: added fluent, wanted, login routes.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Login from "../pages/Login";
+import Fluent from "../pages/Fluent";
+import Wanted from "../pages/Wanted";
 
-export const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>Error</div>,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup/fluent",
+    element: <Fluent />,
+  },
+  { path: "/signup/wanted", element: <Wanted /> },
+]);
