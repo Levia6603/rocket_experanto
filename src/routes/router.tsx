bcf6b505f27730a73ee0f78973991ae1ed67950f
Mosi <mosisu07@gmail.com>
2024-05-06 14:44:12 +0800

    update: added icons & pictures.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Login from "../pages/Login";
+// import Login from "../pages/Login";
 import Fluent from "../pages/Fluent";
-import Wanted from "../pages/Wanted";
+// import Wanted from "../pages/Wanted";
 
 export const router = createBrowserRouter([
   {
@@ -10,13 +10,13 @@ export const router = createBrowserRouter([
     element: <App />,
     errorElement: <div>Error</div>,
   },
-  {
-    path: "/login",
-    element: <Login />,
-  },
+  //   {
+  //     path: "/login",
+  //     element: <Login />,
+  //   },
   {
     path: "/signup/fluent",
     element: <Fluent />,
   },
-  { path: "/signup/wanted", element: <Wanted /> },
+  //   { path: "/signup/wanted", element: <Wanted /> },
 ]);
