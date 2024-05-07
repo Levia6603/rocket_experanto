97c362c6360c8c0b74e952a68be826b964d28cff
Levia <cr1o0127@gmail.com>
2024-05-07 14:11:48 +0800

    feat: add profile page router

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,14 @@
+import { Nav } from "../../components/Nav";
+import Footer from "../../components/Footer";
+
+function Profile() {
+  return (
+    <>
+      <Nav />
+      <h1>456</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default Profile;
