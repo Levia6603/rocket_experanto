017e43496d2e4d63252f03f2a9b52f4802cde6c1
Mosi <mosisu07@gmail.com>
2024-05-10 11:24:10 +0800

    chore: 1. created profileStyle file. 2. modified Nav exrporting way.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Nav } from "../../components/Nav";
+import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
 import {
   Wrapper,
