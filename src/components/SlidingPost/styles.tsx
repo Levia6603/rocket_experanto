fd925d5551f500d4a851747fafefa7694835f7a6
Mosi <mosisu07@gmail.com>
2024-05-17 14:35:25 +0800

    style: updated SlidingPost.

---
+++ src/components/SlidingPost/styles.tsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: -100%;
+  transition: right 0.5s ease-in-out;
+`;
+
+export const Container = styled.div``;
+
+export const Header = styled.div``;
+
+export const Info = styled.div``;
+
+export const Certifications = styled.div``;
