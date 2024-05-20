5a8f1fb65529ec3aff6090114907e25bf4638335
Mosi <mosisu07@gmail.com>
2024-05-20 14:17:12 +0800

    chore: set router & created index, styles file.

---
+++ src/pages/Exchanging/index.tsx
@@ -0,0 +1,15 @@
+import { Wrapper, Container, Title } from "./styles";
+
+function Exchanging() {
+  return (
+    <>
+      <Wrapper>
+        <Container>
+          <Title>Exchanging</Title>
+        </Container>
+      </Wrapper>
+    </>
+  );
+}
+
+export default Exchanging;
---
+++ src/pages/Exchanging/styles.tsx
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.section`
+  padding: 40px 0;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  max-width: 1320px;
+  padding: 0 12px;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h2`
+  padding-bottom: 2rem;
+`;
