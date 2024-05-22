4879bf73817a87461dbbb3d58b108216377f7c5a
Mosi <mosisu07@gmail.com>
2024-05-22 15:09:18 +0800

    chore: set router & created fullReview file.

---
+++ src/pages/FullReview/index.tsx
@@ -0,0 +1,15 @@
+import { Wrapper, Container, Title } from "./styles";
+
+function FullReview() {
+  return (
+    <>
+      <Wrapper>
+        <Container>
+          <Title>Full Review</Title>
+        </Container>
+      </Wrapper>
+    </>
+  );
+}
+
+export default FullReview;
---
+++ src/pages/FullReview/styles.tsx
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.section`
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1320px;
+  padding: 0 12px;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h2`
+  padding-bottom: 2rem;
+  font-weight: 900;
+`;
