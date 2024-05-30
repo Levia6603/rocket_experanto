09ae693e35445350eda2c579e1be0578e2ab82cb
Mosi <mosisu07@gmail.com>
2024-05-30 22:29:43 +0800

    feat: rendered post data

--- src/pages/FullPost/index.tsx
+++ src/pages/FullPost/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import apiBase from "../../Api";
 import {
   Wrapper,
   Header,
@@ -15,24 +18,74 @@ import {
 } from "./styles";
 import Schedule from "../../components/Schedule";
 import Comments from "../../components/Comments";
-import avatar from "/avatar-80.svg";
 import star from "/profile_box_icons/star-black.png";
 import like from "/profile_box_icons/heart.svg";
 import noCertification from "/no-certification-lg.svg";
 
+interface PostInterface {
+  userName: string;
+  userLocations: string;
+  userAvatar: string;
+  title: string;
+  tags: string;
+  skills: { language: string; languageId: number; goal: string[] }[];
+  startDate: string;
+  learn: { Id: number; Name: string; content: string }[];
+  endDate: string;
+  availabaleHours: any;
+  images: string[];
+  isFavorite: boolean;
+}
+
 function FullPost() {
+  const [post, setPost] = useState({} as PostInterface);
+  const [tags, setTags] = useState(String);
+  const [tagAry, setTagAry] = useState([] as string[]);
+
+  //* 接回 Post List
+  async function getPost(id: number) {
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    try {
+      const post: PostInterface = await axios({
+        method: "GET",
+        url: `${apiBase.GET_POST_LIST}/${id}`,
+        headers: headers,
+      })
+        .then((res) => res.data)
+        .catch((err) => console.log(err));
+      setPost(post);
+      setTags(post.tags);
+      console.log(post);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //*接回 Post List
+  useEffect(() => {
+    getPost(2);
+  }, []);
+
+  useEffect(() => {
+    const tagList = tags.split(",");
+    setTagAry(tagList);
+  }, [tags]);
+
   return (
     <>
       <Wrapper>
         <Container>
           <Header>
             <div>
-              <img src={avatar} alt="" />
+              <img src={post?.userAvatar} alt="" />
             </div>
             <div>
               <div>
-                <h4>Ella Dowson</h4>
-                <p>Taipei</p>
+                <h4>{post?.userName}</h4>
+                <p>{post?.userLocations}</p>
               </div>
               <div>
                 <div title="評價">
@@ -46,35 +99,35 @@ function FullPost() {
 
                 <div title="收藏">
                   <div>
-                    <img src={like} alt="liked" />
+                    {post?.isFavorite ? (
+                      <img src={like} alt="liked" />
+                    ) : (
+                      <img src={like} alt="like" />
+                    )}
                   </div>
-                  <p>liked</p>
+                  <p>{post?.isFavorite ? "已收藏" : "收藏"}</p>
                 </div>
               </div>
             </div>
           </Header>
           <Info>
             <div>
-              <h4>你好</h4>
+              <h4>{post?.title}</h4>
             </div>
             <div>
               <div>
-                <p>posted time</p>
-                <p>2024/05/01</p>
+                <p>發文有效日</p>
+                <p>{`${post?.startDate} - ${post?.endDate}`}</p>
               </div>
               <div>
-                <p>Duration</p>
-                <p>2024/05/01 - 2024/05/01</p>
-              </div>
-              <div>
-                <p>Location</p>
-                <p>Taipei</p>
+                <p>所在城市</p>
+                <p>{post?.userLocations}</p>
               </div>
             </div>
           </Info>
           <Calendar>
             <div>
-              <h6>Available Time</h6>
+              <h6>可交換時間</h6>
             </div>
             <div>
               <Schedule />
@@ -82,20 +135,16 @@ function FullPost() {
           </Calendar>
           <Needs>
             <div>
-              <h6>Needs</h6>
+              <h6>需求</h6>
             </div>
             <div>
               <div>
                 <h6>我想學：</h6>
-                <p>Mandarin</p>
+                <p>{post.learn?.[0].Name || ""}</p>
               </div>
               <div>
                 <h6>我希望可以：</h6>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                  non exercitationem voluptate tempore distinctio delectus rerum
-                  commodi, magni eveniet laboriosam?
-                </p>
+                <p>{post.learn?.[0].content || ""}</p>
               </div>
             </div>
           </Needs>
@@ -106,30 +155,16 @@ function FullPost() {
             <div>
               <div>
                 <h6>教學語言：</h6>
-                <p>English</p>
+                <p>{post?.skills?.[0].language || ""}</p>
               </div>
 
               <div>
-                <p>
-                  <span>1. </span>Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Beatae earum placeat alias quasi a dolores
-                  fugiat optio sint qui ea.
-                </p>
-                <p>
-                  <span>2. </span>Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Beatae earum placeat alias quasi a dolores
-                  fugiat optio sint qui ea.
-                </p>
-                <p>
-                  <span>3. </span>Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Beatae earum placeat alias quasi a dolores
-                  fugiat optio sint qui ea.
-                </p>
-                <p>
-                  <span>4. </span>Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Beatae earum placeat alias quasi a dolores
-                  fugiat optio sint qui ea.
-                </p>
+                {post.skills?.[0].goal?.map((goal, index) => (
+                  <p key={index}>
+                    <span>{index + 1}. </span>
+                    {goal}
+                  </p>
+                ))}
               </div>
             </div>
           </Plans>
@@ -138,22 +173,13 @@ function FullPost() {
               <h6>證書</h6>
             </div>
             <div>
-              <Certification>
-                <img src={noCertification} alt="no certification" />
-                <h6>托福</h6>
-              </Certification>
-              <Certification>
-                <img src={noCertification} alt="no certification" />
-                <h6>托福</h6>
-              </Certification>
-              <Certification>
-                <img src={noCertification} alt="no certification" />
-                <h6>托福</h6>
-              </Certification>
-              <Certification>
-                <img src={noCertification} alt="no certification" />
-                <h6>托福</h6>
-              </Certification>
+              {post?.images?.map((image, index) => (
+                <Certification key={index}>
+                  <img src={image} alt="no certification" />
+                  <h6>托福</h6>
+                </Certification>
+              ))}
+
               <Certification>
                 <img src={noCertification} alt="no certification" />
                 <h6>托福</h6>
@@ -165,11 +191,8 @@ function FullPost() {
               <h6># Tags</h6>
             </div>
             <div>
-              <Tag>#English</Tag>
-              <Tag>#English</Tag>
-              <Tag>#English</Tag>
-              <Tag>#English</Tag>
-              <Tag>#English</Tag>
+              {tags &&
+                tagAry?.map((tag, index) => <Tag key={index}>{tag}</Tag>)}
             </div>
           </Tags>
           <Buttons>
--- src/pages/HomeIndex/index.tsx
+++ src/pages/HomeIndex/index.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import apiBase from "../../Api";
+import { useState } from "react";
 import PostCard from "../../components/PostCard";
 import { PostCards } from "../Home/styles";
 import PageBar from "../../components/PageBar";
@@ -13,36 +11,6 @@ function HomeIndex() {
   function handleSelect(el: string) {
     setSelectArea(el);
   }
-  const [postList, setPostList] = useState([]);
-
-  //* 接回 Post List
-  async function getPostList() {
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    };
-    try {
-      const postList = await axios({
-        method: "GET",
-        url: apiBase.GET_POST_LIST,
-        headers: headers,
-      })
-        .then((res) => res.data)
-        .catch((err) => console.log(err));
-      setPostList(postList);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  //*接回 Post List
-  useEffect(() => {
-    getPostList();
-  }, []);
-  useEffect(() => {
-    console.log(postList);
-  }, [postList]);
 
   return (
     <>
