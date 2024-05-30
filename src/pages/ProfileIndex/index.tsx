c094e07c0de55c67ce47db3fcaaeb4cc4ecfdf65
Mosi <mosisu07@gmail.com>
2024-05-30 11:22:50 +0800

    feat: completed data-loading function

--- src/pages/ProfileIndex/index.tsx
+++ src/pages/ProfileIndex/index.tsx
@@ -23,9 +23,19 @@ import avatar from "/nav-profile.png";
 import badge from "/badge.png";
 import noCertification_sm from "/no-certification-sm.svg";
 
+type ProfileType = {
+  avatar: string;
+  gender: string;
+  gendersId: number;
+  image: string[];
+  location: string;
+  locationsId: number;
+  name: string;
+  skills: [{ language: string; languageId: number; goal: string[] }];
+};
 const ProfileIndex = () => {
   const navigate = useNavigate();
-  const [profile, setProfile] = useState({});
+  const [profile, setProfile] = useState({} as ProfileType);
   //* 取得個人資料
   async function getProfile() {
     const headers = {
@@ -35,7 +45,7 @@ const ProfileIndex = () => {
     };
 
     try {
-      const profile = await axios({
+      const profile: ProfileType = await axios({
         method: "GET",
         url: apiBase.GET_PROFILE,
         headers: headers,
@@ -45,7 +55,6 @@ const ProfileIndex = () => {
         })
         .catch((err) => console.log(err));
       setProfile(profile);
-      console.log("Profile loaded successfully");
     } catch (error) {
       console.error(error);
     }
@@ -54,43 +63,40 @@ const ProfileIndex = () => {
   useEffect(() => {
     getProfile();
   }, []);
-  useEffect(() => {
-    console.log(profile);
-  }, [profile]);
 
   return (
     <>
       <User>
-        <img src={avatar} alt="" />
+        <img src={profile?.avatar} alt="" />
         <div>
           <ul>
             <li>
-              <h1>Jane Doe</h1>
+              <h1>{profile?.name}</h1>
             </li>
             <li>
               <Button type="button" onClick={() => navigate("edit")}>
-                Edit my detail
+                編輯
               </Button>
             </li>
           </ul>
           <ul>
             <li>
-              <p>Female</p>
-              <h3>Gender</h3>
+              <p>{profile?.gender}</p>
+              <h3>性別</h3>
             </li>
             <li>
-              <p>Taiwan</p>
-              <h3>Country of residence</h3>
+              <p>{profile?.location}</p>
+              <h3>所在堿市</h3>
             </li>
             <li>
               <p>4.5 (1)</p>
-              <h3>Reviews</h3>
+              <h3>評價</h3>
             </li>
           </ul>
         </div>
       </User>
       <Box>
-        <h2>Achievements</h2>
+        <h2>個人成就</h2>
         <hr />
         <Board>
           <Achiev>
@@ -142,84 +148,44 @@ const ProfileIndex = () => {
       </Box>
       <Box>
         <Title>
-          <h4>Fluent Language</h4>
+          <h4>我可以提供</h4>
         </Title>
         <Cards>
           <ul>
-            <li>
-              <Card>
-                <div>
-                  <h5>English</h5>
-                </div>
-                <div>
-                  <CardItem>
-                    <p>1. </p>
-                    <h6>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Distinctio, at.
-                    </h6>
-                  </CardItem>
-                  <CardItem>
-                    <p>2. </p>
-                    <h6>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Alias, doloribus?
-                    </h6>
-                  </CardItem>
-                </div>
-              </Card>
-            </li>
-            <li>
-              <Card>
-                <div>
-                  <h5>English</h5>
-                </div>
-                <div>
-                  <CardItem>
-                    <p>1. </p>
-                    <h6>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Distinctio, at.
-                    </h6>
-                  </CardItem>
-                  <CardItem>
-                    <p>2. </p>
-                    <h6>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Alias, doloribus?
-                    </h6>
-                  </CardItem>
-                </div>
-              </Card>
-            </li>
+            {profile.skills?.map((skill, index) => (
+              <li key={index}>
+                <Card>
+                  <div>
+                    <h5>{skill.language}</h5>
+                  </div>
+                  <div>
+                    {skill?.goal.map((item, index) => (
+                      <CardItem key={index}>
+                        <p>{index + 1} </p>
+                        <h6>{item}</h6>
+                      </CardItem>
+                    ))}
+                  </div>
+                </Card>
+              </li>
+            ))}
           </ul>
         </Cards>
       </Box>
 
       <Box>
         <CertificationsSection>
-          <h4>Certifications</h4>
+          <h4>資格證照</h4>
           <ul>
-            <li>
-              <CertificationCard>
-                <div>
-                  <img src={noCertification_sm} alt="no certification" />
-                </div>
-                <div>
-                  <h6>TOEFL</h6>
-                </div>
-              </CertificationCard>
-            </li>
-            <li>
-              <CertificationCard>
-                <div>
-                  <img src={noCertification_sm} alt="no certification" />
-                </div>
-                <div>
-                  <h6>TOEFL</h6>
-                </div>
-              </CertificationCard>
-            </li>
+            {profile.image?.map((image, index) => (
+              <li key={index}>
+                <CertificationCard>
+                  <div>
+                    <img src={image} alt="證照" />
+                  </div>
+                </CertificationCard>
+              </li>
+            ))}
           </ul>
         </CertificationsSection>
       </Box>
