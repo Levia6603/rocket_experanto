a5d47c16069f5318d3824f96296b704084cb1ae0
Mosi <mosisu07@gmail.com>
2024-06-02 20:48:54 +0800

    chore: added postId reducer

--- redux/index.ts
+++ redux/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import slidingSlice from "./slidingState/slidingSlice";
 import userSlice from "./user/userSlice";
+import postIdSlice from "./postId/postIdSlice";
 
 const store = configureStore({
   reducer: {
     sliding: slidingSlice,
     user: userSlice,
+    postId: postIdSlice,
   },
 });
 
