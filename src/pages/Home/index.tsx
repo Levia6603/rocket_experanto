8699a146478060c77cf3d355f48363c283e01943
Mosi <mosisu07@gmail.com>
2024-06-02 21:10:34 +0800

    refactor: moved getPost function to SlidingPost page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,47 +5,8 @@ import Footer from "../../components/Footer";
 import ProfileBox from "../../components/ProfileBox";
 import LanguageFilter from "../../components/LanguageFilter";
 import SlidingPost from "../../components/SlidingPost";
-import axios from "axios";
-import apiBase from "../../Api";
-import { useEffect, useState } from "react";
-import { PostInterface } from "../FullPost";
 
 function Home() {
-  const [post, setPost] = useState({} as PostInterface);
-  const [tags, setTags] = useState(String);
-  const [tagAry, setTagAry] = useState([] as string[]);
-  //* 接回 Post List
-  async function getPost(id: number) {
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    };
-    try {
-      const post: PostInterface = await axios({
-        method: "GET",
-        url: `${apiBase.GET_POST}/${id}`,
-        headers: headers,
-      })
-        .then((res) => res.data)
-        .catch((err) => console.log(err));
-      setPost(post);
-      setTags(post.tags || "");
-      console.log(post);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  //*接回 Post List
-  useEffect(() => {
-    getPost(2);
-  }, []);
-
-  useEffect(() => {
-    const tagList = tags.split(",");
-    setTagAry(tagList);
-  }, [tags]);
-
   return (
     <>
       <Nav />
@@ -59,7 +20,7 @@ function Home() {
             <LanguageFilter />
           </RightSection>
         </Container>
-        <SlidingPost post={post} tags={tags} tagAry={tagAry} />
+        <SlidingPost />
       </Wrapper>
       <Footer />
     </>
