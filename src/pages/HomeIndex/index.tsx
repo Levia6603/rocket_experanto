eb1712adf8f215fb813736377353785456691fc0
Mosi <mosisu07@gmail.com>
2024-06-02 15:21:11 +0800

    feat: added getPostList api

--- src/pages/HomeIndex/index.tsx
+++ src/pages/HomeIndex/index.tsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostCard from "../../components/PostCard";
 import { PostCards } from "../Home/styles";
 import PageBar from "../../components/PageBar";
 import { Wrapper, AreaSelector } from "./styles";
+import apiBase from "../../Api";
+import axios from "axios";
+
+interface PostList {
+  postId?: number;
+  userName?: string;
+  userAvatar?: string;
+  isFavorite?: boolean;
+  title?: string;
+  Learn?: string;
+  skill?: string;
+  content?: string;
+  tags?: string[];
+}
+[];
 
 function HomeIndex() {
   const area = ["Taipei", "Taoyuan", "Taichung", "Tainan", "Kaohsiung"];
@@ -11,6 +26,35 @@ function HomeIndex() {
   function handleSelect(el: string) {
     setSelectArea(el);
   }
+  const [postList, setPostList] = useState<PostList | null>({} as PostList);
+
+  async function getPostList() {
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    try {
+      const postList = await axios({
+        method: "GET",
+        url: `${apiBase.GET_POST_LIST}`,
+        headers: headers,
+      })
+        .then((res) => res.data)
+        .catch((err) => console.log(err));
+      setPostList(postList);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //* 接回 Post List
+  useEffect(() => {
+    getPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log(postList);
+  }, [postList]);
 
   return (
     <>
