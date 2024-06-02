6ea43703bcef7c71704be48ea671d4d3f9a9ca47
Mosi <mosisu07@gmail.com>
2024-06-02 23:13:01 +0800

    chore: added pages reducer

--- redux/index.ts
+++ redux/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import slidingSlice from "./slidingState/slidingSlice";
 import userSlice from "./user/userSlice";
 import postIdSlice from "./postId/postIdSlice";
+import pagesSlice from "./pages/pagesSlice";
 
 const store = configureStore({
   reducer: {
     sliding: slidingSlice,
     user: userSlice,
     postId: postIdSlice,
+    pages: pagesSlice,
   },
 });
 
