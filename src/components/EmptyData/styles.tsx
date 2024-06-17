1c6236a85226192c8e1e92c7d14d251ae6691562
Mosi <mosisu07@gmail.com>
2024-06-17 13:39:58 +0800

    style: modified properties

--- src/components/EmptyData/styles.tsx
+++ src/components/EmptyData/styles.tsx
@@ -4,24 +4,15 @@ import empty from "/emptyData.svg";
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
 `;
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 1rem;
   & > div {
+    width: 966px;
+    height: 601px;
     background-image: url(${empty});
     background-repeat: no-repeat;
     background-position: center;
-    background-size: contain;
-    width: 100%;
-    height: 100%;
+    background-size: cover;
   }
 `;
