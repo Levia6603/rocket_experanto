fe27382451fe07323fac81366477cb070b81cd02
Levia <cr1o0127@gmail.com>
2024-06-23 23:23:01 +0800

    style: loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,5 +1,17 @@
+import { Container, Frame, Wrapper } from "./style";
+import logo from "/logo_large.svg";
+
 function Loading() {
-  return <></>;
+  return (
+    <Wrapper>
+      <Container>
+        <Frame>
+          <img src={logo} alt="" />
+          <p>LOADING</p>
+        </Frame>
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default Loading;
