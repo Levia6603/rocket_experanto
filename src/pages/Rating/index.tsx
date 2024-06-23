6e24d368df67703cad51021d36c814a4d6554409
Levia <cr1o0127@gmail.com>
2024-06-23 16:35:16 +0800

    style: rating page

--- src/pages/Rating/index.tsx
+++ src/pages/Rating/index.tsx
@@ -1,6 +1,11 @@
-import { Card, Container, Title } from "./style";
+import { Card, Container, Rate, Stars, Title } from "./style";
+import star from "/profile_box_icons/star-yellow.svg";
+import starOutline from "/profile_box_icons/star.svg";
 
 function Rating() {
+  const avatar = localStorage.getItem("avatar");
+  const name = localStorage.getItem("name");
+
   return (
     <Container>
       <h2>評價內容</h2>
@@ -8,7 +13,113 @@ function Rating() {
         <h3>Title</h3>
         <p>123</p>
       </Title>
-      <Card></Card>
+      <Card>
+        <div>
+          <img src={avatar || ""} alt="" />
+          <p>{name}</p>
+        </div>
+        <ul>
+          <li>教學計劃:</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+        </ul>
+      </Card>
+      <h3>評價</h3>
+      <Rate>
+        <div>
+          <img src={avatar || ""} alt="" />
+          <h4>{name}</h4>
+        </div>
+        <ul>
+          <li>
+            <h5>1. 對於對方的教學規劃，對你的幫助如何?</h5>
+            <Stars>
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </Stars>
+          </li>
+          <li>
+            <h5>2. 對方的教學方式，你喜歡的程度?</h5>
+            <Stars>
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </Stars>
+          </li>
+          <li>
+            <h5>3. 針對這次交換的整體感受如何?</h5>
+            <Stars>
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </Stars>
+          </li>
+          <li>
+            <h5>4. 評價內容: </h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestiae dicta praesentium accusamus velit non laudantium atque
+              minus, ut quos debitis, unde repellat fuga nostrum a dignissimos
+              dolores itaque consectetur culpa?
+            </p>
+          </li>
+        </ul>
+      </Rate>
+      <Rate>
+        <div>
+          <img src={avatar || ""} alt="" />
+          <h4>{name}</h4>
+        </div>
+        <ul>
+          <li>
+            <h5>1. 對於對方的教學規劃，對你的幫助如何?</h5>
+            <Stars>
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </Stars>
+          </li>
+          <li>
+            <h5>2. 對方的教學方式，你喜歡的程度?</h5>
+            <Stars>
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </Stars>
+          </li>
+          <li>
+            <h5>3. 針對這次交換的整體感受如何?</h5>
+            <Stars>
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+            </Stars>
+          </li>
+          <li>
+            <h5>4. 評價內容: </h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestiae dicta praesentium accusamus velit non laudantium atque
+              minus, ut quos debitis, unde repellat fuga nostrum a dignissimos
+              dolores itaque consectetur culpa?
+            </p>
+          </li>
+        </ul>
+      </Rate>
     </Container>
   );
 }
--- src/pages/Rating/style.tsx
+++ src/pages/Rating/style.tsx
@@ -7,6 +7,10 @@ export const Container = styled.div`
     font-size: 32px;
     font-weight: 700;
   }
+  h3 {
+    font-size: 32px;
+    font-weight: 700;
+  }
 `;
 
 export const Title = styled.div`
@@ -24,5 +28,81 @@ export const Title = styled.div`
 `;
 
 export const Card = styled.div`
+  display: flex;
+  margin: 16px;
+  margin-bottom: 56px;
+  border: 1px solid #5e5e5e;
+  border-radius: 4px;
+  background-color: #fff;
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+    width: 96px;
+    padding: 16px 8px;
+    text-align: center;
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 80px;
+      object-fit: cover;
+    }
+    p {
+      font-size: 20px;
+      font-weight: 700;
+    }
+  }
+  & > ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 8px;
+    padding: 8px;
+  }
+`;
+
+export const Rate = styled.div`
+  margin: 24px 16px;
   padding: 16px;
+  background-color: #fff;
+  border: 1px solid #5e5e5e;
+  border-radius: 4px;
+  div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    padding: 16px 0;
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50px;
+      object-fit: cover;
+    }
+    h4 {
+      font-size: 20px;
+      font-weight: 700;
+    }
+  }
+  & > ul {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 8px;
+    h5 {
+      font-size: 16px;
+      font-weight: 700;
+    }
+    p {
+      margin-top: 8px;
+      color: #454545;
+    }
+  }
+`;
+
+export const Stars = styled.ul`
+  display: flex;
+  gap: 12px;
+  margin-top: 4px;
+  padding: 0 16px;
 `;
