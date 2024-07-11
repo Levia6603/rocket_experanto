b2f829e2a3c1e09ed6fb821244c82c51860aeb30
Levia <cr1o0127@gmail.com>
2024-07-11 17:08:52 +0800

    fix: pageBar switch error

--- src/components/PageBar/index.tsx
+++ src/components/PageBar/index.tsx
@@ -23,17 +23,19 @@ function PageBar() {
   };
   //* 上一頁
   const handlePrevious = () => {
-    if (presentPage < 1 || presentPage > pageAmount) {
+    if (presentPage <= 1) {
       return;
     } else {
+      setPresentPage(presentPage - 1);
       dispatch(setPage(presentPage - 1));
     }
   };
   //* 下一頁
   const handleNext = () => {
-    if (presentPage < 1 || presentPage > pageAmount) {
+    if (presentPage < 1 || presentPage >= pageAmount) {
       return;
     } else {
+      setPresentPage(presentPage + 1);
       dispatch(setPage(presentPage + 1));
     }
   };
